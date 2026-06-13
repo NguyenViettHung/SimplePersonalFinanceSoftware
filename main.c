f609a6381746d536e027f44608a333eea18067ac
Nguyen Viet Hung <nguyenviethung30960@gmail.com>
2026-06-13 18:09:40 +0700

    Cap nhat cau truc du an

---
+++ main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "giao_dich.h"
+#include "khai_bao.h"
+#include "file_io.h"
+#include "menu.h"
+#include "ngan_sach.h"
+#include "bao_cao.h"
+
+
+int main() {
+    napDuLieu();
+	while(True)
+	{
+		hienMenu();
+		//nhap lua chon
+		//nhap input chuc nang
+		//xuat output
+	}
+	luuDuLieu();
+	giaiPhongBoNho();
+
+	return 0;
+}
