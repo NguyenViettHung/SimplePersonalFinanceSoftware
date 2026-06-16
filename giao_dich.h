678770d6b837ce3dad02ce7556cff7a4328579b2
thhaaaoooo2006 <thhaaaoooo2006@gmail.com>
2026-06-16 07:52:30 +0700

    Đã đổi lại tên biến

--- giao_dich.h
+++ giao_dich.h
@@ -13,12 +13,12 @@
 void xoaBuffer(void);
 int soSanhNgay (struct GiaoDich a, struct GiaoDich b);
 void hoanVi (struct GiaoDich* a, struct GiaoDich* b);
-void insertionSortGiaoDich (struct GiaoDich* mang, int soLuong);
-int timKiemGiaoDich (struct GiaoDich mang[], int soLuong, int ngay_bd, int thang_bd, int nam_bd, int ngay_kt, int thang_kt, int nam_kt, int ma_dm_loc, int mangKetQua[]);
-void inKetQuaTimKiem (struct GiaoDich mang[], int mangKetQua[], int soKetQua);
+void insertionSortGiaoDich (struct GiaoDich* mang, int so_luong);
+int timKiemGiaoDich (struct GiaoDich mang[], int so_luong, int ngay_bd, int thang_bd, int nam_bd, int ngay_kt, int thang_kt, int nam_kt, int ma_dm_loc, int mang_ket_qua[]);
+void inKetQuaTimKiem (struct GiaoDich mang[], int mang_ket_qua[], int so_ket_qua);
 void tinhLaiSoDu(int so_tien_ns_hien_tai);
 void capNhatSoDu (int delta);
-void tinhTongThuChi (int loai_gd_loc, int ngay_loc, int thang_loc, int nam_loc, int ma_dm_loc, int *tongThu, int *tongChi);
+void tinhtongThuChi (int loai_gd_loc, int ngay_loc, int thang_loc, int nam_loc, int ma_dm_loc, int *tong_thu, int *tong_chi);
 struct GiaoDich nhapGiaoDich();
 void themGiaoDich        (int so_tien_gd,
                   int ngay, int thang, int nam,
