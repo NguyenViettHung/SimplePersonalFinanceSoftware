270f2388db1c1b46e9f8555410bfa14c2e116b92
DoaNHuuVieT-code <vdoanhuu8@gmail.com>
2026-06-22 12:08:20 +0700

    Update khai_bao.h

--- khai_bao.h
+++ khai_bao.h
@@ -8,14 +8,14 @@ typedef struct DanhMuc {
     int ma_dm;         // mã danh mục
     char ten_dm[50];   // tên danh mục
     int han_muc;       // hạn mức theo phần trăm
-    int han_muc_tien;
-    int han_muc_tien_goc;
+    long long han_muc_tien;
+    long long han_muc_tien_goc;
 } DanhMuc;
 
 // 2. Cấu trúc dữ liệu cho ngân sách
 typedef struct NganSach {
     char ma_ns[15];    // mã ngân sách: "NS" + năm + tháng VD: NS202605
-    int so_tien_ns;    // số tiền tương ứng với ngân sách
+    long long so_tien_ns;    // số tiền tương ứng với ngân sách
     int thang;         // tháng ứng với ngân sách
     int nam;           // năm ứng với ngân sách
 } NganSach;
@@ -23,7 +23,7 @@ typedef struct NganSach {
 // 3. Cấu trúc dữ liệu cho giao dịch
 typedef struct GiaoDich {
     char ma_gd[15];    // mã giao dịch: số nguyên tăng dần
-    int so_tien_gd;    // số tiền tương ứng với giao dịch
+    long long so_tien_gd;    // số tiền tương ứng với giao dịch
     int ngay;          // ngày giao dịch
     int thang;         // tháng giao dịch
     int nam;           // năm giao dịch
@@ -33,4 +33,4 @@ typedef struct GiaoDich {
     char ghi_chu[100]; // ghi chú
 } GiaoDich;
 
-#endif /* KHAI_BAO_H */
+#endif
